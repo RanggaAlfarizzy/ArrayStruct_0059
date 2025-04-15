a56ee155ff5161c9d2041e7dc96cad5f0a3ab434
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-04-15 15:47:58 +0800

    Memasukkan Data Data Mahasiswa Dan Membuat Struktur untuk Tabel Mahasiswa

--- struct.cpp
+++ struct.cpp
@@ -6,3 +6,25 @@ struct mahasiswa{
     string nama;
     string alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20240140140001";
+    mhs.nama = "Abra";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukan Alamat = ";
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+}
