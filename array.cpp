0b23b1e5614d6323c704cbd9e1ecb6e9fcddd575
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-04-15 15:25:31 +0800

    mengisi array dengan loop for

--- array.cpp
+++ array.cpp
@@ -12,6 +12,11 @@ int main (){
     cout << "Skor ke 3 = " << skor[2] << endl;
     //mengisi array pada lokasi/index tertentu
     skor[2] = 25;
-    cout << "Skor ke 3 = " << skor[2] << endl;
+    cout << "Skor ke 3 = " << skor[2] << endl;    
+    //mengisi array dengan loop for
+    for(int i = 0; i < 3; i++){
+        cout << "Nama Mahasiswa ke-" << i+1 << " = ";
+        cin >> nama[i];
+    }
 
 }
